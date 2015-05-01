4b3dfd481d3354e22a0813986e1172fcf0ee3cfd
Christopher Dunn <cdunn@pacificbiosciences.com>
2015-05-01 15:46:50 -0700

    phred 24 <- 20

--- fake_fastq.py
+++ fake_fastq.py
@@ -2,12 +2,14 @@ import sys
 import sys, os
 from pbcore.io import FastqIO, FastaIO
 
+phred = 24 # was 20
+
 def main(fasta_name, ofile):
     reader = FastaIO.FastaReader(fasta_name)
     writer = FastqIO.FastqWriter(ofile)
 
     for record in reader:
-        writer.writeRecord(record.header, record.sequence, [20]*len(record.sequence))
+        writer.writeRecord(record.header, record.sequence, [phred]*len(record.sequence))
 
 if __name__ == '__main__':
     iname, oname = sys.argv[1:3]
