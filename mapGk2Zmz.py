9765c8427dbec1b6d345a2560df4b04f56a96a23
Christopher Dunn <cdunn@pacificbiosciences.com>
2015-05-31 12:10:32 -0700

    full read descr, not just zmw

--- mapGk2Zmz.py
+++ mapGk2Zmz.py
@@ -11,23 +11,24 @@
 """
 import re, sys
 
-re_fastqUIDmap = re.compile(r'[0-9]+\s+([0-9]+)\s+[^/]+/([0-9]+)/')
+re_fastqUIDmap = re.compile(r'[0-9]+\s+([0-9]+)\s+([^/]+/[0-9]+/[\S]+)')
 
 def line2pair(line):
     """
     >>> line2pair('100000000004    4       m000_000/3/90596_91636')
-    [4, 3]
+    ('4', 'm000_000/3/90596_91636')
     """
     mo = re_fastqUIDmap.search(line)
-    return map(int, mo.groups())
+    return mo.groups()
 def main():
     for line in sys.stdin:
         uid, zmw = line2pair(line)
-        sys.stdout.write('%d %d\n' %(uid, zmw))
+        sys.stdout.write('%s %s\n' %(uid, zmw))
 def test():
     import doctest
     doctest.testmod()
 if __name__=="__main__":
     if len(sys.argv) > 1:
         test()
+        sys.exit(0)
     main()
